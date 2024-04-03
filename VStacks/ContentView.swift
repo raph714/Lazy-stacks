82a1b58e0b0a50661f31c9a811f357d03214ebde
Raphael Defranco <>
2024-04-03 10:28:54 -0700

    changes

--- VStacks/ContentView.swift
+++ VStacks/ContentView.swift
@@ -9,20 +9,44 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    let contentItems: Int = 100
+    let contentItems: Int = 10000
+
+    @ObservedObject var viewModel = SomeVM()
 
     var body: some View {
-        ScrollView {
-            VStack {
-                ForEach(1...contentItems, id: \.self) { count in
-                    if count % 2 == 0 {
-                        VStackContent(rows: 20, index: count)
-                    } else {
-                        HStackContent(columns: 20, index: count)
+        VStack {
+            Button {
+                viewModel.update()
+            } label: {
+                Text("update")
+            }
+
+            let _ = Self._printChanges()
+
+            ScrollView {
+                LazyVStack {
+                    ForEach(viewModel.names, id: \.self) { name in
+                        HStackContent(columns: 3, index: 1, name: name)
+                        //                    if count % 2 == 0 {
+                        //                        VStackContent(rows: 20, index: count)
+                        //                    } else {
+                        //                        HStackContent(columns: 20, index: count)
+                        //                    }
                     }
                 }
             }
         }
+    }
+}
+
+class SomeVM: ObservableObject {
+    @Published var names: [String] = ["1"]
+    private var num = 1
+
+    func update() {
+        num += 1
+//        names.append("\(num)")
+        names.insert(contentsOf: ["\(num)"], at: 0)
     }
 }
 
@@ -33,11 +57,12 @@ struct ContentView: View {
 struct VStackContent: View {
     let rows: Int
     let index: Int
-    
+    let name: String
+
     var body: some View {
         LazyVStack {
             ForEach(1...rows, id: \.self) { count in
-                ImageView(index: count, container: index)
+                ImageView(index: count, container: index, title: name)
             }
         }
     }
@@ -46,12 +71,13 @@ struct VStackContent: View {
 struct HStackContent: View {
     let columns: Int
     let index: Int
-    
+    let name: String
+
     var body: some View {
         ScrollView(.horizontal) {
             LazyHStack {
                 ForEach(1...columns, id: \.self) { count in
-                    ImageView(index: count, container: index)
+                    ImageView(index: count, container: index, title: name)
                 }
             }
         }
@@ -61,21 +87,23 @@ struct HStackContent: View {
 struct ImageView: View {
     var index: Int
     var containerIndex: Int
-    
-    init(index: Int, container: Int) {
+    var title: String
+
+    init(index: Int, container: Int, title: String) {
         print("View init: Container \(container) Row \(index)")
         self.index = index
         self.containerIndex = container
+        self.title = title
     }
     
     var body: some View {
         VStack {
             let _ = Self._printChanges()
-            Image("sample")
-                .resizable()
-                .scaledToFit()
-                .frame(height: 200)
-                .clipped()
+            Text("View init: Container \(containerIndex) Row \(index)")
+            Text(title)
+            Color(red: Double.random(in: 0...1), green: Double.random(in: 0...1), blue: Double.random(in: 0...1))
+                .frame(width: 200, height: 200)
+                .cornerRadius(8)
         }
     }
 }
